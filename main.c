e0bdd245567607acd8bccf104cfa656b77d63bf2
Deresh-divakar <115335220+Deresh-divakar@users.noreply.github.com>
2022-10-08 10:29:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the amount:");
+    scanf("%d",&a);
+    if(a%5==0)
+    {
+        printf("enter the number of ticket:");
+        scanf("%d",&b);
+    }
+    else
+    {
+        printf("enter the valid amount\n");
+    }
+    if(a%5==0)
+    {
+    c=a-(b*5);
+    printf("balance:%d\n",c);
+    } 
+    else if(c=-c);
+    {
+        printf("pay the balance amount");
+    }
+
+    
+}
